8504019a286cb297e7bea96d71f05bd5e2cb7487
Junhee-lee <sk5514224@gmail.com>
2025-03-03 15:37:07 +0900

    [feat] modify Response class and make MovieDetailRepositoryImpl

--- app/src/main/java/com/github/junhee8649/jetmovie/utils/Response.kt
+++ app/src/main/java/com/github/junhee8649/jetmovie/utils/Response.kt
@@ -1,7 +1,7 @@
 package com.github.junhee8649.jetmovie.utils
 
-sealed class Response<out T> {
-    data class Success<out T>(val data: T) : Response<T>()
-    data class Error(val error: Throwable?) : Response<Nothing>()
-    data object Loading : Response<Nothing>()
+sealed class Response<T>{
+    class Success<T>(val data:T): Response<T>()
+    class Error<T>(val error:Throwable?,val data:T? = null): Response<T>()
+    class Loading<T>: Response<T>()
 }
